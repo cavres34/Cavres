cac050d06f2b4ee33b450a01ba0a8e75c7bb89c7
cavres34 <129806883+cavres34@users.noreply.github.com>
2024-03-13 15:57:35 +0300

    Update song.py

--- AlexaMusic/utils/inline/song.py
+++ AlexaMusic/utils/inline/song.py
@@ -28,7 +28,7 @@ def song_markup(_, vidid):
         [
             InlineKeyboardButton(
                 text="🌻 sᴜᴩᴩᴏʀᴛ 🌻",
-                url="https://t.me/Alexa_Help",
+                url="",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
